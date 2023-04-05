bfd558c1dafa82c87a875b2412e695e50f763477
Jeffrey W. Bullard <jwbullard@tamu.edu>
2023-04-05 17:08:05 -0500

    GEMS3K libraries and resources are now all locally built and installed

--- src/GEMS3K-standalone/install.sh
+++ src/GEMS3K-standalone/install.sh
@@ -6,11 +6,12 @@ threads=1
 BRANCH_GEMS3K=trunk
 BuildType=Release
 #BuildType=Debug
-InstallPrefix=/usr/local
+InstallPrefix=../Resources
 #InstallPrefix=/home/sveta/tmp
 workfolder=${PWD}
 
 
+mkdir -p $InstallPrefix
 mkdir -p build
 cd build
 cmake .. -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_BUILD_TYPE=$BuildType -DCMAKE_INSTALL_PREFIX=$InstallPrefix 
